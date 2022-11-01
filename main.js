f55d7ced5f70f9601b1b6f989b19bf96cf2228fb
= <=>
2022-11-01 13:43:37 +0100

    Botones sliders

--- main.js
+++ main.js
@@ -1,45 +1,82 @@
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '500595ca64msh5dd7af70324f577p10826ejsndbfcf09cb154',
-// 		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-// 	}
-// };
+const slider = document.querySelector(".row__slider");
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+const indicators = document.querySelectorAll(".indicator");
 
-// fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-// 	.then(response => response.json())
-//     .then(data => {
-//         const list = data.d;
+let baseSliderWidth = slider.offsetWidth;
+let activeIndex = 0; // the current page on the slider
 
-//         list.map((item) => {
-       
-//             const name = item.l;
-//             //const poster = item.i.imageUrl;
-//             const movie = `<h2>${name}</h2>`;
-//             document.querySelector('.movies').innerHTML += movie;
-//         })
-//     })
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+// Fill the slider with all the movies in the "movies" array
+function populateSlider() {
+  movies.forEach((image) => {
+    // Clone the initial movie thats included in the html, then replace the image with a different one
+    const newMovie = document.getElementById("movie0");
+    let clone = newMovie.cloneNode(true);
+    let img = clone.querySelector("img");
+    img.src = image.src;
 
-
-
-const API = "https://api.themoviedb.org/3";
-
-function get(path) {
-
-        return fetch(API + path, {
-          headers: {
-            Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
-            "Content-Type": "application/json;charset=utf-8",
-          },
-        })
-        .then((result) => result.json())
-        .then(data => {
-            console.log(data)
-        });
-
-        
-         
+    slider.insertBefore(clone, slider.childNodes[slider.childNodes.length - 1]);
+  });
 }
+
+populateSlider();
+populateSlider();
+
+// delete the initial movie in the html
+const initialMovie = document.getElementById("movie0");
+initialMovie.remove();
+
+// Update the indicators that show which page we're currently on
+function updateIndicators(index) {
+  indicators.forEach((indicator) => {
+    indicator.classList.remove("active");
+  });
+  let newActiveIndicator = indicators[index];
+  newActiveIndicator.classList.add("active");
+}
+
+// Scroll Left button
+btnLeft.addEventListener("click", (e) => {
+  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
+    .width;
+  let scrollDistance = movieWidth * 6; 
+
+  slider.scrollBy({
+    top: 0,
+    left: -scrollDistance,
+    behavior: "smooth",
+  });
+  activeIndex = (activeIndex - 1) % 3;
+  console.log(activeIndex);
+  updateIndicators(activeIndex);
+});
+
+// Scroll Right button
+btnRight.addEventListener("click", (e) => {
+  let movieWidth = document.querySelector(".movie").getBoundingClientRect()
+    .width;
+  let scrollDistance = movieWidth * 6; 
+
+  console.log(`movieWidth = ${movieWidth}`);
+  console.log(`scrolling right ${scrollDistance}`);
+
+  if (activeIndex == 2) {
+    populateSlider();
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = 0;
+    updateIndicators(activeIndex);
+  } else {
+    slider.scrollBy({
+      top: 0,
+      left: +scrollDistance,
+      behavior: "smooth",
+    });
+    activeIndex = (activeIndex + 1) % 3;
+    console.log(activeIndex);
+    updateIndicators(activeIndex);
+  }
+});
